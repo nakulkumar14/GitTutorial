83b5153d931d04ef1976ee17c5cb14dc7a10c719
nakulkumar14 <nakulkumar14.09@gmail.com>
2016-07-07 22:12:39 +0530

    introduced x

--- src/com/git/tutorial/Main.java
+++ src/com/git/tutorial/Main.java
@@ -1,8 +1,14 @@
 package com.git.tutorial;
 
+// Main class
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        // mastering git.
+        int x = 25;
+        System.out.println("x : "+x);
+        x = x*2;
+        System.out.println("x : "+x);
     }   
 }
