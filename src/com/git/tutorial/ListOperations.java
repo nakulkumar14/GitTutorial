2b831be51d2e2617d65745950d1ddcd871ab7dc7
Nakul Kumar <nakul.k@bankbazaar.com>
2016-07-07 21:59:14 +0530

    List created

---
+++ src/com/git/tutorial/ListOperations.java
@@ -0,0 +1,14 @@
+package com.git.tutorial;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by nakulkumar on 7/7/16.
+ */
+public class ListOperations {
+
+    public static void main(String[] args) {
+        List<Integer> coins = new ArrayList<>();
+    }
+}
--- src/com/git/tutorial/Main.java
+++ src/com/git/tutorial/Main.java
@@ -3,6 +3,8 @@ package com.git.tutorial;
 public class Main {
 
     public static void main(String[] args) {
+        int a = 10;
         System.out.println("Hello World!");
-    }   
+        System.out.println(a);
+    }
 }
